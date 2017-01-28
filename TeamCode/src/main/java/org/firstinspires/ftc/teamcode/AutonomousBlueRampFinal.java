6f0c34bbc14ad5e1b5a3219b2b03125f59489d57
masaru-ryumae <mryumae+github@gmail.com>
2017-01-28 15:20:30 -0500

    AutonomousBlueRampFinal is the functionalized version of AutonomousBlueRamp GOLD final version.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueRampFinal.java
@@ -0,0 +1,180 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/*
+ * This OpMode was written for the Vuforia Basics video. This demonstrates basic principles of
+ * using Vuforia in FTC.
+ */
+@Autonomous(name = "Blue Ramp")
+public class AutonomousBlueRampFinal extends LinearOpMode
+{
+
+    HardwareBrainybot robot           = new HardwareBrainybot();
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    static final double     FORWARD_SPEED = -0.6;
+    static final double     SLOWER_FORWARD_SPEED = -0.3;
+    static final double     TURN_SPEED    = -0.5;
+
+
+    public void runOpMode() throws InterruptedException
+    {
+         /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Hello Brainy Bots. Ready to run!");    //
+        telemetry.update();
+
+
+        waitForStart();
+
+
+        // Turn right
+        turnRight(FORWARD_SPEED, 1.2);
+
+        // Go forward
+        goForward(FORWARD_SPEED, 0.5);
+
+        // Turn right 45 degrees
+        turnRight(FORWARD_SPEED, 1.2);
+
+        // Go forward
+        goForward(FORWARD_SPEED, 0.5);
+
+        // Release ball for 2 seconds
+        handleBall(TURN_SPEED, 2.0);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);
+        idle();
+
+        while(opModeIsActive())
+        {
+            // Send telemetry and idle to let hardware catch up
+            telemetry.update();
+            idle();
+        }
+    }
+
+    public void goStop()
+    {
+        // This function stops all the moving motors.
+        robot.armMotor.setPower(0);
+        robot.tableMotor.setPower(0);
+        robot.leftMotor.setPower(0);
+        robot.rightMotor.setPower(0);
+        robot.bottomArm.setPower(0);
+    }
+
+    public void goForward(double speed, double duration) throws InterruptedException
+    {
+        // This function allows robot to go forward with speed, time duration argument.
+
+        robot.armMotor.setPower(speed);
+        robot.tableMotor.setPower(speed);
+        robot.leftMotor.setPower(speed);
+        robot.rightMotor.setPower(speed);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < duration)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            idle();
+        }
+
+        // Stop
+        goStop();
+    }
+
+    public void handleBall(double speed, double duration) throws InterruptedException
+    {
+        // This function allows robot to go forward with speed, time duration argument.
+
+        robot.bottomArm.setPower(speed);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < duration)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            idle();
+        }
+
+        // Stop
+        goStop();
+    }
+
+    public void turnRight(double speed, double duration) throws InterruptedException
+    {
+        // This function allows robot to turn right with speed, time duration argument.
+        robot.armMotor.setPower(speed);
+        robot.tableMotor.setPower(speed);
+        //robot.leftMotor.setPower(speed);
+        //robot.rightMotor.setPower(speed);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.2)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            idle();
+        }
+
+        // Stop
+        goStop();
+    }
+
+    public void slideRight(double speed, double duration) throws InterruptedException
+    {
+        // Slide right
+        //robot.armMotor.setPower(0.5);
+        //robot.tableMotor.setPower(-0.5);
+        //robot.leftMotor.setPower(0.5);
+        //robot.rightMotor.setPower(-0.5);
+        robot.armMotor.setPower(speed);
+        robot.tableMotor.setPower(-speed);
+        robot.leftMotor.setPower(speed);
+        robot.rightMotor.setPower(-speed);
+
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < duration)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            idle();
+        }
+
+        goStop();
+    }
+
+    public void slideLeft(double speed, double duration) throws InterruptedException
+    {
+        // Slide left
+        robot.armMotor.setPower(-speed);
+        robot.tableMotor.setPower(speed);
+        robot.leftMotor.setPower(-speed);
+        robot.rightMotor.setPower(speed);
+
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < duration)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            idle();
+        }
+
+        goStop();
+    }
+}
