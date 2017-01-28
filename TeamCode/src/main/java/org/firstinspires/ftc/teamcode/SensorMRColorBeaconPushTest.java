d4029a1731014e20a8cb509689cf415d23dbcb62
masaru-ryumae <mryumae+github@gmail.com>
2017-01-27 21:42:21 -0500

    ColorBeaconPushTest sliding works ok to see the second image. Initial vision recog not working.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorMRColorBeaconPushTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorMRColorBeaconPushTest.java
@@ -42,6 +42,18 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
 /*
  *
  * This is an example LinearOpMode that shows how to use
@@ -59,17 +71,47 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 //@Disabled
 public class SensorMRColorBeaconPushTest extends LinearOpMode {
 
-  ColorSensor colorSensor;    // Hardware Device Object
-  HardwareBrainybot robot           = new HardwareBrainybot();
-  private ElapsedTime runtime = new ElapsedTime();
-  OpticalDistanceSensor odsSensor;  // Hardware Device Object
+    VuforiaLocalizer vuforiaLocalizer;
+    VuforiaLocalizer.Parameters parameters;
+    VuforiaTrackables visionTargets;
+    VuforiaTrackable target;
+    VuforiaTrackableDefaultListener listener;
 
-  static final double     FORWARD_SPEED = -0.1;
-  boolean buttonPushed = false;
+    OpenGLMatrix lastKnownLocation;
+    OpenGLMatrix phoneLocation;
+
+    public static final String VUFORIA_KEY = "AbeG4Kr/////AAAAGYlYzgmqaUCvl9MEIXe5q7VG+bqPbYy/jH9ZIRe+MDld1JqQEnOy1ljD1xH4lUXpV5DRpXq3iD0LcgRxjm+mTWaIxZ7jB2GroVg6Nn/HbRwnDNTWqC3UBjNAuGjAlUUbMg/CC69vQXKcZl11f97ly/RIj2leGI5MimrUsaTqIcNOQe6UUnkof1LFcpT+18Z7OhqWeRnJJDh9krwMceY1TNW7IwgB+vCrFp25jeQJF9cwclsnieT/NHhjdratCGWhCdU2FHe6mQiO+pjcT2X0suJPdtwomWCVqGDg6clj0A/yHUOrC5YHJ/RROMvAidn1Uo0a1OXL2nguV0jVA/rUeqIEYxTvS5OV0ePxeXlcrAhS"; // Insert your own key here
+
+
+    ColorSensor colorSensor;    // Hardware Device Object
+    HardwareBrainybot robot           = new HardwareBrainybot();
+    private ElapsedTime runtime = new ElapsedTime();
+    OpticalDistanceSensor odsSensor;  // Hardware Device Object
+
+    static final double SLIDE_SPEED = -0.5; // SLIDE_SPEED always Negative value
+    static final double FORWARD_SPEED = -0.1;
+
+    static final double WHEELS_X = 472.1;
+    static final double WHEELS_Y = 422.1;
+    static final double WHEELS_Z = 100.1;
+
+    // Robot's X, Y, Z coordinate values
+    double robotX = 0.0;
+    double robotY = 0.0;
+    double robotBearing = 0.0;
+
+    boolean buttonPushed = false;
 
 
   @Override
   public void runOpMode() throws InterruptedException {
+      setupVuforia();
+      sleep(3000);
+      visionTargets.activate();
+
+      // We don't know where the robot is, so set it to the origin
+      // If we don't include this, it would be null, which would cause errors later on
+      lastKnownLocation = createMatrix(0, 0, 0, 0, 0, 0);
 
     // hsvValues is an array that will hold the hue, saturation, and value information.
     float hsvValues[] = {0F,0F,0F};
@@ -96,6 +138,7 @@ public class SensorMRColorBeaconPushTest extends LinearOpMode {
 
     // Set the LED in the beginning
     colorSensor.enableLed(bLedOn);
+      odsSensor.enableLed(false);
 
     robot.init(hardwareMap);
 
@@ -103,15 +146,93 @@ public class SensorMRColorBeaconPushTest extends LinearOpMode {
     telemetry.addData("Status", "Hello Brainy Bots. Ready to run!");    //
     telemetry.update();
 
+      // Get initial vision info
+      // Ask the listener for the latest information on where the robot is
+      OpenGLMatrix latestLocation = listener.getUpdatedRobotLocation();
+
+      // The listener will sometimes return null, so we check for that to prevent errors
+    if (latestLocation != null) {
+          lastKnownLocation = latestLocation;
+          // Then you can extract the positions and angles using the getTranslation and getOrientation methods.
+          VectorF trans = lastKnownLocation.getTranslation();
+          Orientation rot = Orientation.getOrientation(lastKnownLocation, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+          // Robot position is defined by the standard Matrix translation (x and y)
+          robotX = trans.get(0);
+          robotY = trans.get(1);
+
+          // Robot bearing (in Cartesian system) position is defined by the standard Matrix z rotation
+          robotBearing = rot.thirdAngle;
+          if (robotBearing < 0) {
+              robotBearing = 360 + robotBearing;
+          }
+    }
+    else {
+          telemetry.addData("Pos   ", "Unknown");
+    }
+
     // After Init is pressed, this keeps updating the light values
     while (!(isStarted() || isStopRequested())) {
         telemetry.addData("ODS Raw",    odsSensor.getRawLightDetected());
+        telemetry.addData("robotX",   robotX);
+        telemetry.addData("robotY",   robotY);
         telemetry.update();
         idle();
     }
 
     // wait for the start button to be pressed.
     waitForStart();
+
+
+
+
+
+     // Vision target acquired, move then to the preknown location before it loses frame
+      robot.armMotor.setPower(FORWARD_SPEED);
+      robot.tableMotor.setPower(FORWARD_SPEED);
+      robot.leftMotor.setPower(FORWARD_SPEED);
+      robot.rightMotor.setPower(FORWARD_SPEED);
+      while (opModeIsActive() && (robotX < WHEELS_X)) { // GOOD to stop close to recognize color
+          //while (odsSensor.getRawLightDetected() < 0.1) {
+          if (latestLocation != null) {
+              lastKnownLocation = latestLocation;
+              // Then you can extract the positions and angles using the getTranslation and getOrientation methods.
+              VectorF trans = lastKnownLocation.getTranslation();
+              Orientation rot = Orientation.getOrientation(lastKnownLocation, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+              // Robot position is defined by the standard Matrix translation (x and y)
+              robotX = trans.get(0);
+              robotY = trans.get(1);
+
+              // Robot bearing (in Cartesian system) position is defined by the standard Matrix z rotation
+              robotBearing = rot.thirdAngle;
+              if (robotBearing < 0) {
+                  robotBearing = 360 + robotBearing;
+              }
+          }
+          else {
+              telemetry.addData("Pos   ", "Unknown");
+          }
+
+
+
+          telemetry.addData("RobotX", robotX);
+          telemetry.addData("Status", "In first while loop");
+          telemetry.update();
+          //sleep(10000);
+
+      }
+
+      // STOP to recognize color.
+      // GOOD, without these stop, the robot does not stop from above.
+      robot.armMotor.setPower(0.0);
+      robot.tableMotor.setPower(0.0);
+      robot.leftMotor.setPower(0.0);
+      robot.rightMotor.setPower(0.0);
+
+
+
+
+
+
 
     // START: First staright move close to the button where it can recognize color
       robot.armMotor.setPower(FORWARD_SPEED);
@@ -141,8 +262,8 @@ public class SensorMRColorBeaconPushTest extends LinearOpMode {
       sleep(5000);
       // END: First staright move close to the button where it can recognize color
 
-      // START: Recognize Color
-      if ((colorSensor.red() >= 3) && (colorSensor.blue() == 0) &&
+      // START: Recognize RED Color
+      if ((colorSensor.red() >= 2) && (colorSensor.blue() == 0) &&
               (buttonPushed == false)) {
           telemetry.addData("Status", "RED DETECTED");
           telemetry.update();
@@ -153,7 +274,8 @@ public class SensorMRColorBeaconPushTest extends LinearOpMode {
           robot.leftMotor.setPower(FORWARD_SPEED);
           robot.rightMotor.setPower(FORWARD_SPEED);
 
-          // 0.40 is about hitting the button on beacon
+          // 0.40 is about hitting the button on beacon, if the distance sensor is right hand
+          // side beacon detection, then this 0.40 is too much, about 0.20 would work.
           while (opModeIsActive() && (odsSensor.getRawLightDetected() < 0.40)) { // GOOD to stop close to recognize color
               //while (odsSensor.getRawLightDetected() < 0.1) {
               telemetry.addData("ODS Raw",    odsSensor.getRawLightDetected());
@@ -171,12 +293,30 @@ public class SensorMRColorBeaconPushTest extends LinearOpMode {
           robot.rightMotor.setPower(0.0);
           // END: Bump the button!!
       }
-      else {
-          telemetry.addData("Status", "RED NOT DETECTED");
+      else { //Must be BLUE Color. Slide and bump the button.
+          telemetry.addData("Status", "RED NOT DETECTED, sliding to RIGHT!");
           telemetry.update();
           sleep(5000);
+          slideRight(SLIDE_SPEED, 0.5);
+          sleep(1000);
+          // Then bump button
+          goForward(FORWARD_SPEED, 0.3);
+
       }
       // END: Recognize Color
+
+
+      // Go back a bit after pressing the button
+      telemetry.addData("Status", "Backing up");
+      telemetry.update();
+      sleep(3000);
+      goForward(-FORWARD_SPEED, 0.2);
+
+      // Slide left to detect the next Beacon
+      telemetry.addData("Status", "Sliding LEFT!!");
+      telemetry.update();
+      sleep(3000);
+      slideLeft(SLIDE_SPEED, 3.5);
 /*
       robot.armMotor.setPower(FORWARD_SPEED);
       robot.tableMotor.setPower(FORWARD_SPEED);
@@ -491,4 +631,130 @@ public class SensorMRColorBeaconPushTest extends LinearOpMode {
       idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
     }
   }
+
+    public void goStop()
+    {
+        // This function stops all the moving motors.
+        robot.armMotor.setPower(0);
+        robot.tableMotor.setPower(0);
+        robot.leftMotor.setPower(0);
+        robot.rightMotor.setPower(0);
+    }
+
+    public void goForward(double speed, double duration) throws InterruptedException
+    {
+        // This function allows robot to go forward with speed, time duration argument.
+
+        robot.armMotor.setPower(speed);
+        robot.tableMotor.setPower(speed);
+        robot.leftMotor.setPower(speed);
+        robot.rightMotor.setPower(speed);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < duration)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            idle();
+        }
+
+        // Stop
+        goStop();
+    }
+
+    public void turnRight(double speed) throws InterruptedException
+    {
+        // This function allows robot to turn right with speed, time duration argument.
+        robot.armMotor.setPower(speed);
+        robot.tableMotor.setPower(speed);
+        //robot.leftMotor.setPower(speed);
+        //robot.rightMotor.setPower(speed);
+        //runtime.reset();
+        //while (opModeIsActive() && (runtime.seconds() < 1.2)) {
+        //    telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+        //    telemetry.update();
+        //    idle();
+        //}
+
+        // Stop
+        //goStop();
+    }
+
+    public void slideRight(double speed, double duration) throws InterruptedException
+    {
+        // Slide right
+        //robot.armMotor.setPower(0.5);
+        //robot.tableMotor.setPower(-0.5);
+        //robot.leftMotor.setPower(0.5);
+        //robot.rightMotor.setPower(-0.5);
+        robot.armMotor.setPower(speed);
+        robot.tableMotor.setPower(-speed);
+        robot.leftMotor.setPower(speed);
+        robot.rightMotor.setPower(-speed);
+
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < duration)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            idle();
+        }
+
+        goStop();
+    }
+
+    public void slideLeft(double speed, double duration) throws InterruptedException
+    {
+        // Slide left
+        robot.armMotor.setPower(-speed);
+        robot.tableMotor.setPower(speed);
+        robot.leftMotor.setPower(-speed);
+        robot.rightMotor.setPower(speed);
+
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < duration)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+            idle();
+        }
+
+        goStop();
+    }
+
+    public void setupVuforia()
+    {
+        // Setup parameters to create localizer
+        parameters = new VuforiaLocalizer.Parameters(org.firstinspires.ftc.teamcode.R.id.cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        vuforiaLocalizer = ClassFactory.createVuforiaLocalizer(parameters);
+
+        // These are the vision targets that we want to use
+        // The string needs to be the name of the appropriate .xml file in the assets folder
+        visionTargets = vuforiaLocalizer.loadTrackablesFromAsset("FTC_2016-17");
+
+        // Setup the target to be tracked
+        target = visionTargets.get(0); // 0 corresponds to the wheels target
+        target.setName("Wheels Target");
+        target.setLocation(createMatrix(0, 500, 0, 90, 0, 90));
+
+        // Set phone location on robot
+        phoneLocation = createMatrix(0, 225, 0, 90, 0, 0);
+
+        // Setup listener and inform it of phone information
+        listener = (VuforiaTrackableDefaultListener) target.getListener();
+        listener.setPhoneInformation(phoneLocation, parameters.cameraDirection);
+    }
+
+    // Creates a matrix for determining the locations and orientations of objects
+    // Units are millimeters for x, y, and z, and degrees for u, v, and w
+    public OpenGLMatrix createMatrix(float x, float y, float z, float u, float v, float w)
+    {
+        return OpenGLMatrix.translation(x, y, z).
+                multiplied(Orientation.getRotationMatrix(
+                        AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, u, v, w));
+    }
+
+    // Formats a matrix into a readable string
+    public String formatMatrix(OpenGLMatrix matrix)
+    {
+        return matrix.formatAsTransform();
+    }
 }
